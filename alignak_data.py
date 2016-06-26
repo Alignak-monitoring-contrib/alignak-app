4e6fd550a8f9714ebe848669b8e2282533753114
Algorys <Estrada Matthieu>
2016-06-26 09:17:58 +0200

    Add hosts in menu

--- alignak_data.py
+++ alignak_data.py
@@ -4,7 +4,7 @@ url = 'http://94.76.229.155:90'
 headers = {'Content-Type': 'application/json'}
 params = {'username': 'admin', 'password': 'admin'}
 
-def alignak_backend_data():
+def alignak_backend_auth():
 
     # Token
     response = requests.post(url + '/login', json=params, headers=headers)
@@ -12,25 +12,24 @@ def alignak_backend_data():
     token = resp['token']
 
     # Basic auth
-    auth = requests.auth.HTTPBasicAuth(token, '')
+    backend_auth = requests.auth.HTTPBasicAuth(token, '')
+    return backend_auth
 
+def get_host_state(authentication):
     # get hosts
     hosts = requests.get(
         url + '/livestate?where={"type":"host"}',
-        auth=auth
+        auth=authentication
     )
     s = hosts.json()
 
     data = {}
     for host in s['_items']:
         data[host['name']] = host['state']
-
-    print(data)
-
-def get_alignak_service():
-    alignak_backend_data()
-
-
+    return data
 
 if __name__ == "__main__":
-    get_alignak_service()
+    auth = alignak_backend_auth()
+    data = get_host_state(auth)
+
+    print(data)
--- appalignak.py
+++ appalignak.py
@@ -1,8 +1,7 @@
 import signal, os
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
-import alignak_data
-
+import alignak_data as ad
 
 APPINDICATOR_ID = 'appalignak'
 img = os.path.abspath('images/alignak.png')
@@ -13,17 +12,28 @@ def main():
     indicator.set_menu(build_menu())
     gtk.main()
 
+def alignak_link_menu(menu, value):
+    item = gtk.MenuItem(value)
+    menu.append(item)
+
 def build_menu():
     menu = gtk.Menu()
+
+    auth = ad.alignak_backend_auth()
+    data = ad.get_host_state(auth)
+    for key, v in data.items():
+        value = key + ' -> ' + v
+        alignak_link_menu(menu, value)
+
     item_quit = gtk.MenuItem('Quit')
     item_quit.connect('activate', quit_app)
     menu.append(item_quit)
+
     menu.show_all()
     return menu
 
 def quit_app(source):
     gtk.main_quit()
-
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
