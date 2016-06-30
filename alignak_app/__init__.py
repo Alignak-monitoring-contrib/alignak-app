ef0e9fcc28aa9e05434586403eebfaa4c22e5f02
Matthieu Estrada <m.estrada@alpi.fr>
2016-06-30 13:51:51 +0200

    Make __init__.py empty

--- alignak_app/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-"""
-    Alignak App
-    This module is a Python AppIndicator used Alignak-Backend-Client to connect to Alignak-Backend and Alignak-Webui.
-"""
