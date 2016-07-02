832b5a4013ade8b8ab7bb6fd7619f689017927f9
Algorys <Estrada Matthieu>
2016-07-02 23:47:56 +0200

    Cleaning config files

--- test/setup_test.sh
+++ test/setup_test.sh
@@ -8,16 +8,17 @@ BASE_PATH=$(dirname "$THIS_PATH")
 
 cd $BASE_PATH
 
+echo ' --------- Update and Install packages ... --------- '
 sudo apt-get update
 sudo apt-get install -qq python-gi gir1.2-gtk-3.0 ibus gir1.2-appindicator3-0.1 gir1.2-notify-0.7 gir1.2-glib-2.0
 
-echo 'Upgrade pip ...'
+echo '--------- Upgrade pip ... --------- '
 pip install --upgrade pip
 
 # install prog AND tests requirements :
-echo 'Installing application requirements ...'
+echo '--------- Installing application requirements ... --------- '
 pip install -r requirements.txt
-echo 'Installing application in development mode ...'
+echo '--------- Installing application in development mode ... --------- '
 pip install -e .
-echo 'Installing tests requirements ...'
+echo '--------- Installing tests requirements ... --------- '
 pip install --upgrade -r test/requirements.txt
