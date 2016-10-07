7512f93ad2dda93a630f23b0fadd7846a9070b2d
Matthieu Estrada <ttamalfor@gmail.com>
2016-10-07 15:36:49 +0200

    Clean launch scripts

--- etc/bin/launch.py
+++ etc/bin/launch.py
@@ -20,7 +20,7 @@
 # along with (AlignakApp).  If not, see <http://www.gnu.org/licenses/>.
 
 """
-    Launch is launching application.
+    Launch is launching Alignak-app.
 """
 
 import os
@@ -39,11 +39,11 @@ def launch():
     """
 
     # Actually, we must verify we are in DESKTOP_SESSION or not
-    # try:
-    #     os.environ['DESKTOP_SESSION']
-    # except KeyError as e:
-    #     logger.critical('You must be in desktop session to launch Alignak-App : ' + str(e))
-    #     sys.exit()
+    try:
+        os.environ['DESKTOP_SESSION']
+    except KeyError as e:
+        logger.critical('You must be in desktop session to launch Alignak-App : ' + str(e))
+        sys.exit()
 
     App().run()
 
