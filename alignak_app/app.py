364d0fabd0cfa090f65f480059e149c856939836
Estrada Matthieu <ttamalfor@gmail.com>
2016-11-23 23:48:40 +0100

    Remove unused import

--- alignak_app/app.py
+++ alignak_app/app.py
@@ -31,7 +31,6 @@ from logging import getLogger
 from alignak_app.tray_icon import TrayIcon
 from alignak_app.notifier import AppNotifier
 from alignak_app.utils import get_image_path
-from alignak_app.utils import set_app_config
 from alignak_app.utils import create_logger
 
 try:
