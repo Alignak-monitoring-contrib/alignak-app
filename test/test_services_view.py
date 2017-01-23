d61cb95fcc34172514737543058e6ed890ae9a74
Matthieu Estrada <ttamalfor@gmail.com>
2017-01-23 14:43:30 +0100

    Add Unit Tests for ServicesView
    Ref #127 services_view.py

--- test/test_services_view.py
+++ test/test_services_view.py
@@ -25,6 +25,7 @@ import unittest2
 
 from alignak_app.core.utils import init_config
 from alignak_app.synthesis.services_view import ServicesView
+from alignak_app.synthesis.service import Service
 from alignak_app.core.action_manager import ActionManager
 from alignak_app.core.backend import AppBackend
 
@@ -47,7 +48,6 @@ class TestServicesView(unittest2.TestCase):
 
     action_manager = ActionManager(app_backend)
 
-
     @classmethod
     def setUpClass(cls):
         """Create QApplication"""
@@ -67,3 +67,62 @@ class TestServicesView(unittest2.TestCase):
 
         self.assertIsNotNone(under_test.layout)
 
+    def test_update_buttons(self):
+        """Update Services Buttons"""
+
+        under_test = ServicesView(self.action_manager, self.app_backend)
+        under_test.display_services(None, {'name': ''})
+
+        # If service is acknowledged, acknowledge_btn is not enable
+        test_service_ack = {
+            'name': 'My Service',
+            'ls_state': 'WARNING',
+            'ls_last_check': 0.0,
+            'ls_output': 'Output of the service',
+            'ls_acknowledged': True,
+            'ls_downtimed': False,
+        }
+
+        service_ack = Service()
+        service_ack.initialize(test_service_ack)
+
+        under_test.update_service_buttons(service_ack)
+
+        self.assertFalse(service_ack.acknowledge_btn.isEnabled())
+        self.assertTrue(service_ack.downtime_btn.isEnabled())
+
+        # If service is downtimed, downtime_btn is not enable
+        test_service_down = {
+            'name': 'My Service',
+            'ls_state': 'WARNING',
+            'ls_last_check': 0.0,
+            'ls_output': 'Output of the service',
+            'ls_acknowledged': False,
+            'ls_downtimed': True,
+        }
+
+        service_down = Service()
+        service_down.initialize(test_service_down)
+
+        under_test.update_service_buttons(service_down)
+
+        self.assertTrue(service_down.acknowledge_btn.isEnabled())
+        self.assertFalse(service_down.downtime_btn.isEnabled())
+
+        # If service is not OK, acknowledged and downtimed, buttons are enable
+        test_service_not_ok = {
+            'name': 'My Service',
+            'ls_state': 'WARNING',
+            'ls_last_check': 0.0,
+            'ls_output': 'Output of the service',
+            'ls_acknowledged': False,
+            'ls_downtimed': False,
+        }
+
+        service_not_ok = Service()
+        service_not_ok.initialize(test_service_not_ok)
+
+        under_test.update_service_buttons(service_not_ok)
+
+        self.assertTrue(service_not_ok.acknowledge_btn.isEnabled())
+        self.assertTrue(service_not_ok.downtime_btn.isEnabled())
