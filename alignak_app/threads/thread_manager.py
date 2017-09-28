f09b0078eb1be3a9d0c0d3af2a3df09b90bf453a
Matthieu Estrada <ttamalfor@gmail.com>
2017-09-28 10:33:31 +0200

    Remove unused import

--- alignak_app/threads/thread_manager.py
+++ alignak_app/threads/thread_manager.py
@@ -28,7 +28,6 @@ import sys
 from PyQt5.Qt import QApplication  # pylint: disable=no-name-in-module
 from PyQt5.Qt import QTimer, QObject  # pylint: disable=no-name-in-module
 
-from alignak_app.core.backend import app_backend
 from alignak_app.core.data_manager import DataManager
 from alignak_app.core.locales import init_localization
 from alignak_app.core.utils import init_config
