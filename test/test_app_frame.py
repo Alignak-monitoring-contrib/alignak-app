c236bb2705b091785b7cbe96a218fba682a7b8c1
Estrada Matthieu <ttamalfor@gmail.com>
2017-10-18 08:14:03 +0200

    Fix AppQFrame Unit Tests

--- test/test_app_frame.py
+++ test/test_app_frame.py
@@ -25,7 +25,7 @@ import unittest2
 from PyQt5.QtWidgets import QApplication
 
 from alignak_app.core.utils import init_config
-from alignak_app.widgets.frames import AppQFrame
+from alignak_app.widgets.frames.app_frame import AppQFrame
 
 
 class TestAppQFrame(unittest2.TestCase):
