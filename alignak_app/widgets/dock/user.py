0f7acdbce070b82ece55db21e20108395fd77b00
Estrada Matthieu <ttamalfor@gmail.com>
2017-10-19 23:33:54 +0200

    Improve user QWidget and its css
    Ref #210

--- alignak_app/widgets/dock/user.py
+++ alignak_app/widgets/dock/user.py
@@ -36,8 +36,8 @@ from alignak_app.widgets.dialogs.token import TokenQDialog
 from alignak_app.widgets.dialogs.password import PasswordQDialog
 from alignak_app.widgets.dialogs.user_notes import UserNotesQDialog
 
-from PyQt5.Qt import QGridLayout, QVBoxLayout, QLineEdit  # pylint: disable=no-name-in-module
-from PyQt5.Qt import QIcon, Qt, QHBoxLayout, QLabel, QWidget  # pylint: disable=no-name-in-module
+from PyQt5.Qt import QGridLayout, QVBoxLayout  # pylint: disable=no-name-in-module
+from PyQt5.Qt import QIcon, Qt, QLabel, QWidget  # pylint: disable=no-name-in-module
 from PyQt5.Qt import QPushButton, QCheckBox  # pylint: disable=no-name-in-module
 
 logger = getLogger(__name__)
@@ -68,9 +68,9 @@ class UserQWidget(QWidget):
         self.app_widget = None
         self.host_notif_state = None
         self.service_notif_state = None
-        self.password_btn = None
+        self.password_btn = QPushButton()
         self.token_btn = QPushButton()
-        self.notes_btn = None
+        self.notes_btn = QPushButton()
 
     def initialize(self):
         """
@@ -86,53 +86,14 @@ class UserQWidget(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        layout.addWidget(self.get_main_informations_widget())
+        layout.addWidget(self.get_informations_widget())
         layout.addWidget(self.get_notifications_widget())
-
-    def get_main_informations_widget(self):
-        """
-        Create and return informations QWidget
-
-        :return: informations QWidget
-        :rtype: QWidget
-        """
-
-        informations_widget = QWidget()
-        informations_layout = QHBoxLayout()
-        informations_widget.setLayout(informations_layout)
-
-        informations_layout.addWidget(self.get_informations_widget())
-        informations_layout.addWidget(self.get_notes_widget())
-
-        return informations_widget
 
     def get_informations_widget(self):
         """
-        Create and return QWidget with main informations
+        Create and return QWidget with user informations
 
         :return: main QWidget
-        :rtype: QWidget
-        """
-
-        main_user_widget = QWidget()
-        main_layout = QGridLayout()
-        main_user_widget.setLayout(main_layout)
-
-        main_title = QLabel(_('User informations:'))
-        main_title.setObjectName("title")
-        main_layout.addWidget(main_title, 0, 0, 1, 4)
-        main_layout.addWidget(get_frame_separator(), 1, 0, 1, 4)
-
-        main_layout.addWidget(self.get_identity_widget(), 2, 0, 1, 2)
-        main_layout.addWidget(self.get_rights_widget(), 2, 2, 1, 2)
-
-        return main_user_widget
-
-    def get_identity_widget(self):
-        """
-        Return informations QWidget
-
-        :return: information QWidget
         :rtype: QWidget
         """
 
@@ -140,85 +101,96 @@ class UserQWidget(QWidget):
         info_layout = QGridLayout()
         information_widget.setLayout(info_layout)
 
-        # Realm
+        # Main title
+        main_title = QLabel(_('User informations:'))
+        main_title.setObjectName("title")
+        info_layout.addWidget(main_title, 0, 0, 1, 6)
+        info_layout.setAlignment(main_title, Qt.AlignCenter)
+        info_layout.addWidget(get_frame_separator(), 1, 0, 1, 6)
+
+        # Realm & Is Admin & Notes title and button
+        line = 2
         realm_title = QLabel(_('<h5>Realm:</h5>'))
         realm_title.setObjectName("subtitle")
-        info_layout.addWidget(realm_title, 0, 0, 1, 1)
-        info_layout.addWidget(self.labels['realm'], 0, 1, 1, 1)
+        info_layout.addWidget(realm_title, line, 0, 1, 1)
+        info_layout.addWidget(self.labels['realm'], line, 1, 1, 1)
 
-        # Role
-        role_title = QLabel(_('<h5>Role:</h5>'))
-        role_title.setObjectName("subtitle")
-        info_layout.addWidget(role_title, 1, 0, 1, 1)
-        info_layout.addWidget(self.labels['role'], 1, 1, 1, 1)
-
-        # Mail
-        mail_title = QLabel(_('<h5>Email:</h5>'))
-        mail_title.setObjectName("subtitle")
-        info_layout.addWidget(mail_title, 2, 0, 1, 1)
-        info_layout.addWidget(self.labels['email'], 2, 1, 1, 1)
-
-        # Alias
-        alias_title = QLabel(_('<h5>Alias:</h5>'))
-        alias_title.setObjectName("subtitle")
-        info_layout.addWidget(alias_title, 3, 0, 1, 1)
-        info_layout.addWidget(self.labels['alias'], 3, 1, 1, 1)
-
-        return information_widget
-
-    def get_rights_widget(self):
-        """
-        Create and return Rights QWidget
-
-        :return: rights QWidget
-        :rtype: QWidget
-        """
-
-        rights_widget = QWidget()
-        rights_layout = QGridLayout()
-        rights_widget.setLayout(rights_layout)
-
-        # Is Admin
         admin_title = QLabel(_('<h5>Administrator:</h5>'))
         admin_title.setObjectName("subtitle")
         admin_title.setMinimumHeight(32)
-        rights_layout.addWidget(admin_title, 1, 0, 1, 1)
+        info_layout.addWidget(admin_title, line, 2, 1, 1)
         self.labels['is_admin'].setFixedSize(14, 14)
         self.labels['is_admin'].setScaledContents(True)
-        rights_layout.addWidget(self.labels['is_admin'], 1, 1, 1, 1)
+        info_layout.addWidget(self.labels['is_admin'], line, 3, 1, 1)
 
-        # Can submit commands
+        notes_title = QLabel(_('Notes:'))
+        notes_title.setObjectName("title")
+        info_layout.addWidget(notes_title, line, 4, 1, 1)
+
+        self.notes_btn.setIcon(QIcon(get_image_path('edit')))
+        self.notes_btn.setToolTip(_("Edit your notes."))
+        self.notes_btn.setObjectName("notes")
+        self.notes_btn.setFixedSize(32, 32)
+        self.notes_btn.clicked.connect(self.patch_data)
+        info_layout.addWidget(self.notes_btn, line, 5, 1, 1)
+        line += 1
+
+        # Role & Can submit commands
+        role_title = QLabel(_('<h5>Role:</h5>'))
+        role_title.setObjectName("subtitle")
+        info_layout.addWidget(role_title, line, 0, 1, 1)
+        info_layout.addWidget(self.labels['role'], line, 1, 1, 1)
+
         command_title = QLabel(_('<h5>Commands:</h5>'))
         command_title.setObjectName("subtitle")
         command_title.setMinimumHeight(32)
-        rights_layout.addWidget(command_title, 2, 0, 1, 1)
+        info_layout.addWidget(command_title, line, 2, 1, 1)
         self.labels['can_submit_commands'].setFixedSize(14, 14)
         self.labels['can_submit_commands'].setScaledContents(True)
-        rights_layout.addWidget(self.labels['can_submit_commands'], 2, 1, 1, 1)
+        info_layout.addWidget(self.labels['can_submit_commands'], line, 3, 1, 1)
 
-        # Password
+        # Create QLabel for notes
+        self.labels['notes'].setText(data_manager.database['user'].data['notes'])
+        self.labels['notes'].setWordWrap(True)
+        self.labels['notes'].setObjectName('notes')
+        info_layout.addWidget(self.labels['notes'], line, 4, 1, 2)
+        info_layout.setAlignment(Qt.AlignTop)
+        line += 1
+
+        # Mail & Password
+        mail_title = QLabel(_('<h5>Email:</h5>'))
+        mail_title.setObjectName("subtitle")
+        info_layout.addWidget(mail_title, line, 0, 1, 1)
+        info_layout.addWidget(self.labels['email'], line, 1, 1, 1)
+
         password_title = QLabel(_('<h5>Password:</h5>'))
         password_title.setObjectName("subtitle")
-        rights_layout.addWidget(password_title, 3, 0, 1, 1)
-        self.password_btn = QPushButton()
+        info_layout.addWidget(password_title, line, 2, 1, 1)
+
         self.password_btn.setObjectName("password")
         self.password_btn.clicked.connect(self.patch_data)
         self.password_btn.setIcon(QIcon(get_image_path('password')))
         self.password_btn.setToolTip(_('Change my password'))
         self.password_btn.setFixedSize(32, 32)
-        rights_layout.addWidget(self.password_btn, 3, 1, 1, 1)
+        info_layout.addWidget(self.password_btn, line, 3, 1, 1)
+        line += 1
 
-        # Token visible only for administrators
+        # Alias & Token (only for administrators)
+        alias_title = QLabel(_('<h5>Alias:</h5>'))
+        alias_title.setObjectName("subtitle")
+        info_layout.addWidget(alias_title, line, 0, 1, 1)
+        info_layout.addWidget(self.labels['alias'], line, 1, 1, 1)
+
         token_title = QLabel(_('<h5>Token:</h5>'))
         token_title.setObjectName("subtitle")
-        rights_layout.addWidget(token_title, 4, 0, 1, 2)
+        info_layout.addWidget(token_title, line, 2, 1, 2)
 
         self.token_btn.setIcon(QIcon(get_image_path('token')))
         self.token_btn.setFixedSize(32, 32)
         self.token_btn.clicked.connect(self.show_token_dialog)
-        rights_layout.addWidget(self.token_btn, 4, 1, 1, 1)
+        info_layout.addWidget(self.token_btn, line, 3, 1, 1)
 
-        return rights_widget
+        return information_widget
 
     @staticmethod
     def show_token_dialog():
@@ -231,42 +203,6 @@ class UserQWidget(QWidget):
         token_dialog.initialize()
 
         token_dialog.exec_()
-
-    def get_notes_widget(self):
-        """
-        Create and return Notes QWidget
-
-        :return: notes QWidget
-        :rtype: QWidget
-        """
-
-        notes_widget = QWidget()
-        notes_layout = QGridLayout()
-        notes_widget.setLayout(notes_layout)
-
-        main_notes_title = QLabel(_('Notes:'))
-        main_notes_title.setObjectName("title")
-        notes_layout.addWidget(main_notes_title, 0, 0, 1, 1)
-        notes_layout.addWidget(get_frame_separator(), 1, 0, 1, 4)
-
-        # Notes button
-        self.notes_btn = QPushButton()
-        self.notes_btn.setIcon(QIcon(get_image_path('edit')))
-        self.notes_btn.setToolTip(_("Edit your notes."))
-        self.notes_btn.setObjectName("notes")
-        self.notes_btn.setFixedSize(32, 32)
-        self.notes_btn.clicked.connect(self.patch_data)
-        notes_layout.addWidget(self.notes_btn, 2, 3, 1, 1)
-
-        # Create QLabel for notes
-        self.labels['notes'].setText(data_manager.database['user'].data['notes'])
-        self.labels['notes'].setWordWrap(True)
-        self.labels['notes'].setObjectName('notes')
-        notes_layout.addWidget(self.labels['notes'], 3, 0, 1, 4)
-
-        notes_layout.setAlignment(Qt.AlignTop)
-
-        return notes_widget
 
     def patch_data(self):
         """
@@ -337,11 +273,9 @@ class UserQWidget(QWidget):
 
         host_notif_widget = self.get_hosts_notif_widget()
         notification_layout.addWidget(host_notif_widget, 0, 0, 1, 1)
-        notification_layout.setAlignment(host_notif_widget, Qt.AlignTop)
 
         services_notif_widget = self.get_services_notif_widget()
         notification_layout.addWidget(services_notif_widget, 0, 1, 1, 1)
-        notification_layout.setAlignment(services_notif_widget, Qt.AlignTop)
 
         return notification_widget
 
@@ -370,8 +304,7 @@ class UserQWidget(QWidget):
             data_manager.database['user'].data['host_notifications_enabled']
         )
         self.host_notif_state.stateChanged.connect(self.enable_notifications)
-        self.host_notif_state.setObjectName('hostactions')
-        self.host_notif_state.setFixedSize(18, 18)
+        self.host_notif_state.setObjectName('host_notifications_enabled')
         host_notif_layout.addWidget(self.host_notif_state, 2, 1, 1, 1)
 
         period_title = QLabel(_("<h5>Notification period:</h5>"))
@@ -418,12 +351,11 @@ class UserQWidget(QWidget):
         state_title.setObjectName("subtitle")
         service_notif_layout.addWidget(state_title, 2, 0, 1, 1)
         self.service_notif_state = QCheckBox()
-        self.service_notif_state.setObjectName('serviceactions')
+        self.service_notif_state.setObjectName('service_notifications_enabled')
         self.service_notif_state.setChecked(
             data_manager.database['user'].data['service_notifications_enabled']
         )
         self.service_notif_state.stateChanged.connect(self.enable_notifications)
-        self.service_notif_state.setFixedSize(18, 18)
         service_notif_layout.addWidget(self.service_notif_state, 2, 1, 1, 1)
 
         period_title = QLabel(_("<h5>Notification period:</h5>"))
@@ -455,39 +387,29 @@ class UserQWidget(QWidget):
 
         """
 
-        check_btn = self.sender()
+        # btn.checkState() equal to 0 or 2
+        notification_enabled = bool(self.sender().checkState())
+        notification_type = self.sender().objectName()
 
-        notification_type = ''
-        if 'hostactions' in check_btn.objectName():
-            notification_type = 'host_notifications_enabled'
-        elif 'serviceactions' in check_btn.objectName():
-            notification_type = 'service_notifications_enabled'
+        data = {notification_type: notification_enabled}
+        headers = {'If-Match': data_manager.database['user'].data['_etag']}
+        endpoint = '/'.join(['user', data_manager.database['user'].item_id])
+
+        patched = app_backend.patch(endpoint, data, headers)
+
+        if patched:
+            data_manager.database['user'].update_data(notification_type, notification_enabled)
+            enabled = 'enabled' if notification_enabled else 'disabled'
+            message = _("Notifications for %ss are %s") % (
+                self.sender().objectName().replace('_notifications_enabled', ''),
+                enabled
+            )
+            send_event('INFO', message)
         else:
-            logger.error('Wrong caller %s', self.sender().objectName())
-
-        if notification_type:
-            # btn.checkState() equal to 0 or 2
-            notification_enabled = bool(check_btn.checkState())
-
-            data = {notification_type: notification_enabled}
-            headers = {'If-Match': data_manager.database['user'].data['_etag']}
-            endpoint = '/'.join(['user', data_manager.database['user'].item_id])
-
-            patched = app_backend.patch(endpoint, data, headers)
-
-            if patched:
-                data_manager.database['user'].update_data(notification_type, notification_enabled)
-                enabled = 'enabled' if notification_enabled else 'disabled'
-                message = _("Notifications for %ss are %s") % (
-                    check_btn.objectName().replace('actions', ''),
-                    enabled
-                )
-                send_event('INFO', message)
-            else:
-                send_event(
-                    'ERROR',
-                    _("Backend PATCH failed, please check your logs !")
-                )
+            send_event(
+                'ERROR',
+                _("Backend PATCH failed, if problem persist, please check your logs !")
+            )
 
     def update_widget(self):
         """
