b7196955c65168f1a3ff31f8b059878072113c0b
Estrada Matthieu <ttamalfor@gmail.com>
2018-03-03 17:54:13 +0100

    Remove unused parameter in AppQFrame

--- alignak_app/qobjects/common/frames.py
+++ alignak_app/qobjects/common/frames.py
@@ -51,14 +51,12 @@ class AppQFrame(QFrame):
         self.child_widget = None
         self.offset = None
 
-    def initialize(self, title, logo=False):
+    def initialize(self, title):
         """
         Initialize the QFrame, with its "title"
 
         :param title: title of frame
         :type title: str
-        :param logo: Display logo or title
-        :type logo: bool
         """
 
         self.setWindowTitle(title)
@@ -68,65 +66,55 @@ class AppQFrame(QFrame):
         main_layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(main_layout)
 
-        main_layout.addWidget(self.get_logo_widget(title, logo))
+        main_layout.addWidget(self.get_title_widget(title))
 
         self.setAttribute(Qt.WA_TransparentForMouseEvents)
 
-    def get_logo_widget(self, title, logo):
+    def get_title_widget(self, title):
         """
-        Return the logo QWidget
+        Return the title QWidget for App windows
 
         :param title: title of frame
         :type title: str
-        :param logo: Display logo or title
-        :type logo: bool
-        :return: logo QWidget
+        :return: a title QWidget
         :rtype: QWidget
         """
 
-        logo_widget = QWidget()
-        logo_widget.setFixedHeight(45)
-        logo_widget.setObjectName('logo')
-        logo_layout = QHBoxLayout()
-        logo_widget.setLayout(logo_layout)
+        title_widget = QWidget()
+        title_widget.setFixedHeight(45)
+        title_widget.setObjectName('logo')
+        title_layout = QHBoxLayout()
+        title_widget.setLayout(title_layout)
 
         logo_label = QLabel()
         logo_label.setObjectName('widget_title')
         logo_label.setAttribute(Qt.WA_TransparentForMouseEvents)
-        if logo:
-            logo_label.setPixmap(QPixmap(settings.get_image('alignak')))
-            logo_label.setFixedSize(120, 30)
-            logo_label.setScaledContents(True)
-
-            logo_layout.addWidget(logo_label)
-        else:
-            logo_label.setText('<h3>%s</h3>' % title)
-
-        logo_layout.addWidget(logo_label)
-        logo_layout.setAlignment(logo_label, Qt.AlignHCenter)
+        logo_label.setText('<h3>%s</h3>' % title)
+        title_layout.addWidget(logo_label)
+        title_layout.setAlignment(logo_label, Qt.AlignHCenter)
 
         minimize_btn = QPushButton()
         minimize_btn.setIcon(QIcon(settings.get_image('minimize')))
         minimize_btn.setFixedSize(22, 22)
         minimize_btn.setObjectName('app_widget')
         minimize_btn.clicked.connect(self.minimize)
-        logo_layout.addWidget(minimize_btn)
+        title_layout.addWidget(minimize_btn)
 
         maximize_btn = QPushButton()
         maximize_btn.setIcon(QIcon(settings.get_image('maximize')))
         maximize_btn.setFixedSize(22, 22)
         maximize_btn.setObjectName('app_widget')
         maximize_btn.clicked.connect(self.minimize_maximize)
-        logo_layout.addWidget(maximize_btn)
+        title_layout.addWidget(maximize_btn)
 
         close_btn = QPushButton()
         close_btn.setIcon(QIcon(settings.get_image('exit')))
         close_btn.setFixedSize(22, 22)
         close_btn.setObjectName('app_widget')
         close_btn.clicked.connect(self.close_widget)
-        logo_layout.addWidget(close_btn)
+        title_layout.addWidget(close_btn)
 
-        return logo_widget
+        return title_widget
 
     def minimize(self):  # pragma: no cover - not testable
         """
